8fc6b379c8dcac656c28cc27b3b7c9dd937164a9
Crillin222 <iuricasagrande@gmail.com>
2022-09-28 17:29:34 -0300

    first commit

---
+++ core/src/com/badlogic/drop/Drop.java
@@ -0,0 +1,27 @@
+package com.badlogic.drop;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class Drop extends ApplicationAdapter {
+	private Texture dropImage;
+	private Texture bucketImage;
+	
+	@Override
+	public void create () {
+		//load the images for the droplet and the bucket, 64x64 pixels each
+		dropImage = new Texture(Gdx.files.internal("droplet.png"));
+		bucketImage = new Texture(Gdx.files.internal("bucket.png"));
+	}
+
+	@Override
+	public void render () {
+		ScreenUtils.clear(1, 0, 0, 1);
+	}
+	
+	@Override
+	public void dispose () {
+	}
+}
