643435540a16eeda50b38f40cad50cde01f780c2
Crillin222 <iuricasagrande@gmail.com>
2022-09-28 17:35:52 -0300

    added camera and spritebatch to the drop class

--- core/src/com/badlogic/drop/Drop.java
+++ core/src/com/badlogic/drop/Drop.java
@@ -2,12 +2,16 @@ package com.badlogic.drop;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class Drop extends ApplicationAdapter {
 	private Texture dropImage;
 	private Texture bucketImage;
+	private OrthographicCamera camera;
+	private SpriteBatch batch;
 	
 	@Override
 	public void create () {
